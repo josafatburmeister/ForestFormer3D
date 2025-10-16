53772f35dddd0f77af5633e8505269ae6d7fde93
Josafat-Mattias Burmeister <josafat-mattias.burm@hpi.de>
2025-10-16 20:15:38 +0200

    fix: data handling when train or val set are empty

--- tools/update_infos_to_v2.py
+++ tools/update_infos_to_v2.py
@@ -201,6 +201,7 @@ def update_scannet_infos(pkl_path, out_dir):
     data_list = mmengine.load(pkl_path)
     print('Start updating:')
     converted_list = []
+    ignore_class_name = set()
     for ori_info_dict in mmengine.track_iter_progress(data_list):
         temp_data_info = get_empty_standard_data_info()
         temp_data_info['lidar_points']['num_pts_feats'] = ori_info_dict[
@@ -220,7 +221,6 @@ def update_scannet_infos(pkl_path, out_dir):
         # TODO support camera
         # np.linalg.inv(info['axis_align_matrix'] @ extrinsic): depth2cam
         anns = ori_info_dict.get('annos', None)
-        ignore_class_name = set()
         if anns is not None:
             temp_data_info['axis_align_matrix'] = anns[
                 'axis_align_matrix'].tolist()
@@ -317,6 +317,7 @@ def update_scannet200_infos(pkl_path, out_dir):
     data_list = mmengine.load(pkl_path)
     print('Start updating:')
     converted_list = []
+    ignore_class_name = set()
     for ori_info_dict in mmengine.track_iter_progress(data_list):
         temp_data_info = get_empty_standard_data_info()
         temp_data_info['lidar_points']['num_pts_feats'] = ori_info_dict[
@@ -336,7 +337,6 @@ def update_scannet200_infos(pkl_path, out_dir):
         # TODO support camera
         # np.linalg.inv(info['axis_align_matrix'] @ extrinsic): depth2cam
         anns = ori_info_dict.get('annos', None)
-        ignore_class_name = set()
         if anns is not None:
             temp_data_info['axis_align_matrix'] = anns[
                 'axis_align_matrix'].tolist()
@@ -394,6 +394,7 @@ def update_forainetv2_infos(pkl_path, out_dir):
     data_list = mmengine.load(pkl_path)
     print('Start updating:')
     converted_list = []
+    ignore_class_name = set()
     for ori_info_dict in mmengine.track_iter_progress(data_list):
         temp_data_info = get_empty_standard_data_info()
         temp_data_info['lidar_points']['num_pts_feats'] = ori_info_dict[
@@ -413,7 +414,6 @@ def update_forainetv2_infos(pkl_path, out_dir):
         # TODO support camera
         # np.linalg.inv(info['axis_align_matrix'] @ extrinsic): depth2cam
         anns = ori_info_dict.get('annos', None)
-        ignore_class_name = set()
         if anns is not None:
             temp_data_info['axis_align_matrix'] = anns[
                 'axis_align_matrix'].tolist()
